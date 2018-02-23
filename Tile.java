e96d263d882275786fcf903b62a2a030aa10f430
chrischapman82 <christopher.chapman8296@gmail.com>
2018-02-23 12:29:25 +1100

    Contains methods to change and get tiles from a map

--- Tile.java
+++ Tile.java
@@ -5,28 +5,8 @@ public class Tile {
     public static final int X_INC = 1;          // for incrementing x and y
     public static final int Y_INC = Globals.planet_width;
 
-    public static int getIndex(long x, long y) {
-        return (int)(x + y*Globals.planet_width);
-    }
-
     public static int getIndex(MapLocation loc) {
         return (int)(loc.getX()*X_INC + loc.getY()*Y_INC);
-    }
-
-    public static int increaseX(int index) {
-        return index + X_INC;
-    }
-
-    public static int decreaseX(int index) {
-        return index-X_INC;
-    }
-
-    public static int increaseY(int index) {
-        return index+Y_INC;
-    }
-
-    public static int decreaseY(int index) {
-        return index-Y_INC;
     }
 
     public static MapLocation getMapLocation(int index) {
