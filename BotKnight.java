f8ab9e2e9d96858e7b42a7319876003e7ffb8d04
chrischapman82 <christopher.chapman8296@gmail.com>
2018-02-23 12:20:32 +1100

    Contains Knight bot logic

--- BotKnight.java
+++ BotKnight.java
@@ -4,8 +4,21 @@ public class BotKnight extends AttackingBot{
 
 
 
-    /*public static void update(Unit unit) {
-        AttackingBot.update(unit);
-    }*/
+    public static void update(Unit unit) {
+
+
+        // attack if possible
+        if (attack(unit, getAttackableEnemies(unit))) {
+
+        }
+
+        if (engage(unit, getViewableEnemies(unit))) {
+            return;
+        }
+
+        if (unit.movementHeat() < 10) {
+            Nav.moveToEnemyBase(unit.id());
+        }
+    }
 
 }
